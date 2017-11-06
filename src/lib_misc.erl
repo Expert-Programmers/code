48e6c104317fb178a50f6cf1a4aa0e36eb49e177
ltoddy <1205446803@qq.com>
2017-11-06 20:48:52 +0800

    归集器: 如果编写一个函数,把某个整数列表一分为二,分别包含原始列表里面的奇数和偶数, odds_and_evens1需要遍历两边列表, odds_and_evens2只需要遍历一遍列表

--- src/lib_misc.erl
+++ src/lib_misc.erl
@@ -2,7 +2,7 @@
 -author("ltoddy").
 
 %% API
--export([for/3, qsort/1, pythag/1, perms/1]).
+-export([for/3, qsort/1, pythag/1, perms/1, odds_and_evens1/1, odds_and_evens2/1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I) | for(I + 1, Max, F)].
@@ -42,3 +42,22 @@ perms(L) -> [[H | T] || H <- L, T <- perms(L--[H])].
 %% ["cats","cast","ctas","ctsa","csat","csta","acts","acst",
 %% "atcs","atsc","asct","astc","tcas","tcsa","tacs","tasc",
 %% "tsca","tsac","scat","scta","sact","satc","stca","stac"]
+
+
+odds_and_evens1(L) ->
+  Odds = [X || X <- L, (X rem 2) =:= 1],
+  Evens = [X || X <- L, (X rem 2) =:= 0],
+  {Odds, Evens}.
+
+%% 2> lib_misc:odds_and_evens1([1,2,3,4,5,6]).
+%% {[1,3,5],[2,4,6]}
+
+odds_and_evens2(L) -> odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H | T], Odds, Evens) ->
+  case (H rem 2) of
+    1 -> odds_and_evens_acc(T, [H | Odds], Evens);
+    0 -> odds_and_evens_acc(T, Odds, [H | Evens])
+  end;
+odds_and_evens_acc([], Odds, Evens) ->
+  {Odds, Evens}.
