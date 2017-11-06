1079835b99e7ba2f171ca510d6fa0093fec8a4c8
ltoddy <1205446803@qq.com>
2017-11-06 19:49:29 +0800

    回文构词

--- src/lib_misc.erl
+++ src/lib_misc.erl
@@ -2,7 +2,7 @@
 -author("ltoddy").
 
 %% API
--export([for/3, qsort/1, pythag/1]).
+-export([for/3, qsort/1, pythag/1, perms/1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I) | for(I + 1, Max, F)].
@@ -31,3 +31,14 @@ pythag(N) ->
 %% [{3,4,5},{4,3,5}]
 %% 2> lib_misc:pythag(30).
 %% [{3,4,5},{4,3,5},{5,12,13},{6,8,10},{8,6,10},{12,5,13}]
+
+
+perms([]) -> [[]];
+perms(L) -> [[H | T] || H <- L, T <- perms(L--[H])].
+
+%% 2> lib_misc:perms("123").
+%% ["123","132","213","231","312","321"]
+%% 3> lib_misc:perms("cats").
+%% ["cats","cast","ctas","ctsa","csat","csta","acts","acst",
+%% "atcs","atsc","asct","astc","tcas","tcsa","tacs","tasc",
+%% "tsca","tsac","scat","scta","sact","satc","stca","stac"]
