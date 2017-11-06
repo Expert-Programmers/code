88c7bd9224d05f2e4ca3771fa637be8a36d8a2eb
ltoddy <1205446803@qq.com>
2017-11-06 19:44:00 +0800

    毕达哥拉斯三元组

--- src/lib_misc.erl
+++ src/lib_misc.erl
@@ -2,7 +2,7 @@
 -author("ltoddy").
 
 %% API
--export([for/3, qsort/1]).
+-export([for/3, qsort/1, pythag/1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I) | for(I + 1, Max, F)].
@@ -15,3 +15,19 @@ qsort([Pivot | T]) ->
 %% [23,6,2,9,27,400,78,45,61,82,14]
 %% 2> lib_misc:qsort(L).
 %% [2,6,9,14,23,27,45,61,78,82,400]
+
+
+pythag(N) ->
+  [
+    {A, B, C} ||
+    A <- lists:seq(1, N),
+    B <- lists:seq(2, N),
+    C <- lists:seq(3, N),
+    A + B + C =< N,
+    A * A + B * B =:= C * C
+  ].
+
+%% 1> lib_misc:pythag(16).
+%% [{3,4,5},{4,3,5}]
+%% 2> lib_misc:pythag(30).
+%% [{3,4,5},{4,3,5},{5,12,13},{6,8,10},{8,6,10},{12,5,13}]
