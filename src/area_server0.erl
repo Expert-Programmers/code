c52476352e4fe2b4ee52af063cb9a63d84cb57fe
ltoddy <1205446803@qq.com>
2017-11-16 21:06:06 +0800

    函数转变为进程 - demo

---
+++ src/area_server0.erl
@@ -0,0 +1,15 @@
+-module(area_server0).
+-author("ltoddy").
+
+%% API
+-export([loop/0]).
+
+loop() ->
+  receive
+    {rectangle, Width, Height} ->
+      io:format("Area of rectangle is ~p~n", [Width * Height]),
+      loop();
+    {{square, Side}} ->
+      io:format("Area of square is ~p~n", [Side * Side]),
+      loop()
+  end.
