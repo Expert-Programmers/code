582ba63a3ec7973b06298959234b7363a53e5a22
ltoddy <1205446803@qq.com>
2017-11-29 20:44:59 +0800

    添加测试

--- src/socket_examples.erl
+++ src/socket_examples.erl
@@ -3,7 +3,7 @@
 -import(lists, [reverse/1]).
 
 %% API
--export([nano_get_url/0, nano_get_url/1]).
+-export([nano_get_url/0, nano_get_url/1, start_nano_server/0, loop/1, receive_data/2, nano_client_eval/1]).
 
 
 nano_get_url() ->
@@ -42,4 +42,15 @@ loop(Socket) ->
       loop(Socket);
     {tcp_closed, Socket} ->
       io:format("Server socket closed~n")
+  end.
+
+nano_client_eval(Str) ->
+  {ok, Socket} = gen_tcp:connect("localhost", 2345, [binary, {packet, 4}]),
+  ok = gen_tcp:send(Socket, term_to_binary(Str)),
+  receive
+    {tcp, Socket, Bin} ->
+      io:format("Client received binary = ~p~n", [Bin]),
+      Val = binary_to_term(Bin),
+      io:format("Client result = ~p~n", [Val]),
+      gen_tcp:close(Socket)
   end.
